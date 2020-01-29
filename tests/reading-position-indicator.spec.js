dbea28f6f98e5abde8e7570b4bd139449c6ab005
Fabian Kägy <mail@fabian-kaegy.de>
2020-01-29 18:33:19 +0100

    add tests

---
+++ tests/reading-position-indicator.spec.js
@@ -0,0 +1,64 @@
+import puppeteer from 'puppeteer';
+
+const APP = 'https://fabiankaegy.github.io/component-reading-position-indicator/demo/demo-2.html';
+const width = 1440;
+const height = 860;
+
+let page;
+let browser;
+
+beforeAll( async () => {
+
+	browser = await puppeteer.launch( {
+		headless: true,
+	} );
+
+	page = await browser.newPage();
+
+	await page.setViewport( {
+		width,
+		height
+	} );
+
+} );
+
+describe( 'General Tests', () => {
+
+	test( 'starts with percentage of 0', async () => {
+
+		// Visit the page in headless Chrome
+		await page.goto( APP );
+
+		const progressElement = await page.$('progress');
+		console.log( progressElement );
+		const percentageHandle = await progressElement.getProperty( 'value' );
+		const percentage = await percentageHandle.jsonValue();
+
+		expect( percentage ).toBe( 0 );
+
+	} );
+
+	test( 'updates the percentage when scrolled', async () => {
+
+		// Visit the page in headless Chrome
+		await page.goto( APP );
+
+		// scroll all the way to the bottom
+		await page.evaluate(() => {
+			window.scrollBy( 0, document.body.scrollHeight )
+		})
+
+		const progressElement = await page.$('progress');
+		console.log( progressElement );
+		const percentageHandle = await progressElement.getProperty( 'value' );
+		const percentage = await percentageHandle.jsonValue();
+
+		expect( percentage ).toBe( 100 );
+
+	} );
+
+} );
+
+afterAll( () => {
+	browser.close();
+} );
